b114ef2aef83ebd296a285b37d3ab565f8e33a0e
Pauli Østerø <poe@acto.dk>
2014-10-24 12:11:33 +0200

    Refactored code working with inherited data to use the new InheritanceDataFacade introduced in Core.

--- Email/MailEventsFacade.cs
+++ Email/MailEventsFacade.cs
@@ -1,77 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 
 using Composite.Data;
 
+using CompositeC1Contrib.Data;
 using CompositeC1Contrib.Email.Data.Types;
 
 namespace CompositeC1Contrib.Email
 {
     public class MailEventsFacade
     {
-        private static readonly MethodInfo CastMethod = typeof(Queryable).GetMethod("Cast", BindingFlags.Static | BindingFlags.Public);
-        private static readonly MethodInfo DeleteMethod;
-
-        private static readonly List<Type> LogTypes = new List<Type>();
-
-        static MailEventsFacade()
-        {
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            foreach (var assembly in assemblies)
-            {
-                try
-                {
-                    var types = assembly.GetTypes()
-                        .Where(t => t.IsInterface
-                            && typeof(IEvent).IsAssignableFrom(t)
-                            && t.GetCustomAttribute<ImmutableTypeIdAttribute>() != null);
-
-                    LogTypes.AddRange(types);
-                }
-                catch { }
-            }
-
-            var methods = typeof(DataFacade).GetMethods(BindingFlags.Static | BindingFlags.Public).Where(m => m.Name == "Delete");
-            foreach (var method in methods)
-            {
-                var genericArguments = method.GetGenericArguments().ToList();
-                if (genericArguments.Count != 1)
-                {
-                    continue;
-                }
-
-                var parameters = method.GetParameters().ToList();
-                if (parameters.Count != 1)
-                {
-                    continue;
-                }
-
-                var parameter = parameters[0];
-                if (!parameter.ParameterType.IsGenericType)
-                {
-                    continue;
-                }
-
-                var genericType = parameter.ParameterType.GetGenericTypeDefinition();
-                if (genericType != typeof(IEnumerable<>))
-                {
-                    continue;
-                }
-
-                genericArguments = parameter.ParameterType.GetGenericArguments().ToList();
-                if (genericArguments.Count != 1)
-                {
-                    continue;
-                }
-
-                DeleteMethod = method;
-
-                break;
-            }
-        }
-
         public static IEnumerable<T> GetEventsOfType<T>(IMailTemplate template) where T : class, IEvent
         {
             using (var data = new DataConnection())
@@ -102,29 +41,12 @@ namespace CompositeC1Contrib.Email
 
         public static IEnumerable<IEvent> GetEvents(Guid messageId)
         {
-            var list = new List<IEvent>();
-            foreach (var type in LogTypes)
-            {
-                var items = DataFacade.GetData(type).Cast<IEvent>().Where(l => l.MailMessageId == messageId);
-
-                list.AddRange(items);
-            }
-
-            return list.OrderBy(l => l.Timestamp);
+            return InheritanceDataFacade.GetData<IEvent>(l => l.MailMessageId == messageId).OrderBy(l => l.Timestamp);
         }
 
         public static void ClearEvents(IMailMessage message)
         {
-            foreach (var type in LogTypes)
-            {
-                var genericCast = CastMethod.MakeGenericMethod(type);
-                var genericDelete = DeleteMethod.MakeGenericMethod(type);
-
-                var items = DataFacade.GetData(type).Cast<IEvent>().Where(l => l.MailMessageId == message.Id);
-                var converted = (IQueryable)genericCast.Invoke(null, new object[] { items });
-
-                genericDelete.Invoke(null, new object[] { converted });
-            }
+            InheritanceDataFacade.Delete<IEvent>(l => l.MailMessageId == message.Id);
         }
     }
 }
