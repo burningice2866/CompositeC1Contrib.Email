964ff36c2a3a4816b4bc78ebc5bf274ced9854e1
Pauli Østerø <pauli@burningice.dk>
2016-07-19 10:49:25 +0200

    Fix copy/paste bug related to changeset a06f6c6cd5ab

--- Email/C1Console/Workflows/EditConfigurableSystemNetMailClientQueueWorkflow.cs
+++ Email/C1Console/Workflows/EditConfigurableSystemNetMailClientQueueWorkflow.cs
@@ -2,11 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Net.Mail;
 
-using Composite.C1Console.Workflow;
-
 namespace CompositeC1Contrib.Email.C1Console.Workflows
 {
-    [AllowPersistingWorkflow(WorkflowPersistingType.Idle)]
     public sealed class EditConfigurableSystemNetMailClientQueueWorkflow : EditMailQueueWorkflow
     {
         public EditConfigurableSystemNetMailClientQueueWorkflow() : base("\\InstalledPackages\\CompositeC1Contrib.Email\\EditConfigurableSystemNetMailClientQueue.xml") { }
