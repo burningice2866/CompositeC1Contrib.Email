0840355dc9b56f19a11b187e0d38d194e3f49a03
Pauli Østerø <pauli@burningice.dk>
2017-05-22 10:21:01 -0200

    Removed obsolete IMailTemplate properties

--- Email/Data/Types/IMailTemplate.cs
+++ Email/Data/Types/IMailTemplate.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+﻿using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
 
 using Composite.Data;
 using Composite.Data.Hierarchy;
@@ -25,16 +23,6 @@ namespace CompositeC1Contrib.Email.Data.Types
         [ImmutableFieldId("07330426-8fbf-40b2-9894-6c3a45abb172")]
         [StoreFieldType(PhysicalStoreFieldType.String, 512, IsNullable = true)]
         string ModelType { get; set; }
-
-        [ImmutableFieldId("c7a4ef88-3c73-4cca-89af-67455bf8e7d9")]
-        [StoreFieldType(PhysicalStoreFieldType.String, 256, IsNullable = true)]
-        [Obsolete("Use IMailTemplateContent.Subject")]
-        string Subject { get; set; }
-
-        [ImmutableFieldId("c52dda12-1ae2-4f32-bfc2-bd971c3cfb9b")]
-        [StoreFieldType(PhysicalStoreFieldType.LargeString, IsNullable = true)]
-        [Obsolete("Use IMailTemplateContent.Body")]
-        string Body { get; set; }
 
         [ImmutableFieldId("44991190-8622-468b-a01b-f37418d78da3")]
         [StoreFieldType(PhysicalStoreFieldType.Boolean)]
